e4b0fe213ad16d84dd20b1fc9e75fe881b7cd27e
Yutama <yutama.budiman@iconagency.com.au>
2024-11-19 21:28:02 +1100

    SPA finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React from 'react'
-import TestGroup from './components/TestGroup'
+import React from 'react';
+import { useFetchTeacher } from './hooks/useFetchSchools';
+import { Divider } from 'primereact/divider';
 import { Button } from 'primereact/button';
-import Card from './components/Card';
+import Table from './components/Table/Table';
 
-import "primereact/resources/themes/lara-light-cyan/theme.css";
+import './App.css';
+import 'primereact/resources/themes/lara-dark-indigo/theme.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const MyApp = () => {
+  const { data, meta, error, currentPage, goToNextPage, goToPreviousPage } = useFetchTeacher();
 
-const myApp = () => {
+  if (error) {
+    console.error('Error fetching data:', error);
+  }
 
   return (
-    <>
-      <Card />
-      <Button label="Click" />
-      <TestGroup />
-    </>
-  )
+    <div className='mt-4 container'>
+      <h1 className='text-center'>Teacher Information</h1>
+      <h2 className='text-center'>School: Wonde Testing School / A1930499544</h2>
+      <Divider />
+      <div className='row justify-content-center'>
+        <div className='col-md-12'>
+          <Table teacherData={data} />
+        </div>
+      </div>
+
+      <div className='mt-3 row d-flex justify-content-between'>
+        <div className='col-md-3'>
+          <Button label="Previous" onClick={goToPreviousPage} disabled={!meta?.pagination?.previous} />
+        </div>
+        <div className='col-md-3 text-end'>
+          <Button label="Next" onClick={goToNextPage} disabled={!meta?.pagination?.next} />
+        </div>
+      </div>
+      <div className='mt-3 row'>
+        <div className='col-md-12 text-end'>
+          <span className='mx-2'>Page {currentPage}</span>
+        </div>
+      </div>
+
+      <Divider />
+    </div>
+  );
 }
 
-export default myApp
+export default MyApp;
